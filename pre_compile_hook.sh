099028becf0a58158fa6b5d30834cf2532f23df6
alissa-tung <alissa-tung@outlook.com>
2022-10-13 11:06:10 +0800

    spec REV

--- pre_compile_hook.sh
+++ pre_compile_hook.sh
@@ -2,11 +2,13 @@
 set -e
 
 RS_SRC_DIR='hstreamdb-rust'
+REV='bfc8f6fe348b6ff8a18910835747a1a29afd2c31'
 
 mkdir -p rs_src
 cd rs_src/
 [ -d $RS_SRC_DIR ] || git clone --recurse-submodules https://github.com/hstreamdb/hstreamdb-rust.git
 cd $RS_SRC_DIR/
+git checkout $REV
 cargo build --release
 mkdir -p ../../priv
 cp target/release/libhstreamdb_erl_nifs.so ../../priv
